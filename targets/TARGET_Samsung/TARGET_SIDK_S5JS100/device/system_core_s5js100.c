90c11460684f68398869018b591f7e66d35708b9
swon.lee <swon.lee@samsung.com>
2019-12-23 10:33:19 +0900

    some fix for license verification
    Signed-off-by: swon.lee <swon.lee@samsung.com>

--- targets/TARGET_Samsung/TARGET_SIDK_S5JS100/device/system_core_s5js100.c
+++ targets/TARGET_Samsung/TARGET_SIDK_S5JS100/device/system_core_s5js100.c
@@ -1,3 +1,18 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2018 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /****************************************************************************
  *
  * Copyright 2019 Samsung Electronics All Rights Reserved.
@@ -16,6 +31,11 @@
  * language governing permissions and limitations under the License.
  *
  ****************************************************************************/
+/* @file  : system_core_s5js100.c
+ * @brief : System core configuration code
+ * @date  : December 2019
+ * @note  : Add system mode and set system time ticker
+ */
 
 #include "cmsis.h"
 #include "os_tick.h"
